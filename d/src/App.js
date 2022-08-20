6c75ababc71e5a4dcaac39f120d3779a1f1dfdc0
rajmishra-47 <81914386+rajmishra-47@users.noreply.github.com>
2022-08-20 18:02:27 +0530

    basic changes

--- d/src/App.js
+++ d/src/App.js
@@ -5,20 +5,35 @@ import React,{useState} from 'react'
 function App() {
 
   const [set,reSet]=useState('');
+  const [set1,reSet1]=useState('');
+  const [set2,reSet2]=useState('');
 
   const a=()=>{
 
-    fetch("/").then((obj)=>obj.text())
+    fetch(`/y/${set}/${set1}`).then((obj)=>obj.text())
     .then((mess)=>{
-      reSet(mess)
+      reSet2(mess)
       console.log(String(mess));
     })
+  }
+
+  const b=(e)=>{
+
+    return reSet(e.target.value)
+  }
+
+  const c=(e)=>{
+
+    return reSet1(e.target.value)
   }
 
   return (
    <>
    <button onClick={a}>Click me</button>
-   <h1>{set}</h1>
+
+   <input type="text" name="" id="" onChange={b} />
+   <input type="text" name="" id="" onChange={c}/>
+<h1>{set2}</h1>
    </>
   );
 }
